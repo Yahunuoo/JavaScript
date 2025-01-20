0a31df27735d422e1e7f9e45daef66f97fb0b51d
Yahunuoo <muhanna.juma@outlook.com>
2025-01-20 19:32:47 +0300

    first commit :)

--- arrays/app.js
+++ arrays/app.js
@@ -1,10 +1,20 @@
-
 const arra1 = [1,2,3,4,5,6,7,8,9,10];
-const arra2 = [11,12,13,14,15,16,17,18,19,20];
+const arra2 = ['a','b','c','d','e','f','g','h','i','j'];
 
 const max = arra1.concat(arra2);
 
+console.log(max.includes('m')); // النتيجة: true
+console.log(max.includes('b'));  // النتيجة: false
+
 console.log(max);
+console.log(max.join(', ')); // Join elements of max array with a comma and space
+
+// const array1 = [1, 2, 3];
+// const array2 = [4, 5, 6];
+// const combinedArray = [...array1, ...array2];
+
+// console.log(combinedArray); // النتيجة: [1, 2, 3, 4, 5, 6]
+// console.log(combinedArray.join(' - ')); // Join elements of combinedArray with a hyphen and space
 
 
 

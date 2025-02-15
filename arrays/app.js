66757f978c7f57e00594a30c38fadec768b28079
Yahunuoo <muhanna.juma@outlook.com>
2025-02-15 13:11:42 +0300

    add content :)

---
+++ arrays/app.js
@@ -0,0 +1,4 @@
+// Adds an element to the end of an array
+let fruits = ["Apple", "Banana", "Orange"]; // Creating an array
+let addfruit = fruits.push("Mango"); // Adding an element to the end of the array
+console.log(fruits); // Output: ["Apple", "Banana", "Orange", "Mango"]
